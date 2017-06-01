ab540a2d6662aba10aa801b7788889f2fa1c057d
LidDongQi <atguigu@yeah.net>
2017-06-01 16:17:12 +0800

    create 001

---
+++ cat.java
@@ -0,0 +1,29 @@
+public class Cat{
+	private Integer age;
+	private String name;
+	private Double weight;
+	
+	public Integer getAge(){
+		return age;
+	}
+	
+	public void setAge(Integer age){
+		this.age=age;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public Double getWeight(){
+		return weight;
+	}
+	
+	public void setWeight(Double weight){
+		this.weight=weight;
+	}
+}
